c3602038d3538285138ae3c11f81aa612f073633
Rafia-here <workswithrafia@gmail.com>
2026-03-01 13:12:24 +0500

    chapter4 completed

--- chapter 2/chapter3/chapter4/app.js
+++ chapter 2/chapter3/chapter4/app.js
@@ -1,34 +1,34 @@
 // Qno.1
-// var variables = "myName , myBirthDate , myEmail";
+var variables = "myName , myBirthDate , myEmail";
 //  Qno.2
-// legal
-// 1.$ 
-// 2._
-//  3.letter
-//  4.allow numbers in center and end 
+legal
+1.$ 
+2._
+ 3.letter
+ 4.allow numbers in center and end 
 // illegal
-// 1.start with number
-//  2.- 
-//  3.space not allow
-//  4.symbol not allwed
-//  5.we dont use reserved keywords like: var, let, function , etc 
+1.start with number
+ 2.- 
+ 3.space not allow
+ 4.symbol not allwed
+ 5.we dont use reserved keywords like: var, let, function , etc 
 //  Qno.3
 // option a
-// 1. a variable name must start with a letter,_,or$ 
-// 2. space are not allowed
-// javascript keywords cannot be used as variables names
-// variable names are case-sensitive.
-// use meaningful and clear names.
+1. a variable name must start with a letter,_,or$ 
+2. space are not allowed
+javascript keywords cannot be used as variables names
+variable names are case-sensitive.
+use meaningful and clear names.
 // option b
-// 1. Letters
-// 2. numbers
-// 3.underscore 
-// 3. dollar sign$
+1. Letters
+2. numbers
+3.underscore 
+3. dollar sign$
 // option c
-// 1. letter
-// 2. underscore
-// 3. dollar$
+1. letter
+2. underscore
+3. dollar$
 // option d
-// sensitive
+sensitive
 // option e
-// Keywords
+Keywords
