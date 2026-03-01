09e2e7f75d44a9452d32344c066f125c186a4683
Rafia-here <workswithrafia@gmail.com>
2026-03-01 13:02:12 +0500

    chapter 3,4 completed

---
+++ chapter 2/chapter3/app.js
@@ -0,0 +1,11 @@
+// Qno.1
+var age = "I am 15 years old";
+alert(age);
+// Qno.2
+var visitor = "You have visited this site 14 times";
+alert(visitor);
+// incompleted
+// Qno.4
+var visitorName = "John doe";
+var productTitle = "T-shirt";
+var quantity = "5";
