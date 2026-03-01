e750a38a899e900045defaff67768afe39001d0a
Rafia-here <workswithrafia@gmail.com>
2026-03-01 13:10:11 +0500

    chapter3 completed

--- chapter 2/app.js
+++ chapter 2/app.js
@@ -1,41 +1,41 @@
 // Qno.1
-var email = "UserName";
-console.log (email);
+// var email = "UserName";
+// console.log (email);
 
 // Qno.2
-var myName = "Waniya";
-console.log (myname);
+// var myName = "Waniya";
+// console.log (myname);
 
 // Qno.3
 // a)
-var titledMessage;
+// var titledMessage;
 // b)
-var message = "Hello World";
-console.log (message);
+// var message = "Hello World";
+// console.log (message);
 // c)
-var message = "Hello World";
-alert(message);
+// var message = "Hello World";
+// alert(message);
 // Qno.4
-var studentBiodata = "Jhone Doe";
-alert(studentBiodata);
+// var studentBiodata = "Jhone Doe";
+// alert(studentBiodata);
 // a)
-var age = "15 years old";
-alert(age);
+// var age = "15 years old";
+// alert(age);
 // b)
-var course = "Certified Mobile Application Development";
-alert(course);
+// var course = "Certified Mobile Application Development";
+// alert(course);
 // Qno.5
-var food = "Pizza \n pizz \n piz \n pi \n p ";
-alert(food);
+// var food = "Pizza \n pizz \n piz \n pi \n p ";
+// alert(food);
 // Qno.6
-var email = "My email address is example@example.com";
-alert(email);
+// var email = "My email address is example@example.com";
+// alert(email);
 // Qno.7
-var book = "I am trying to learn From the book A smarter way to learn JavaScript";
-alert(book);
+// var book = "I am trying to learn From the book A smarter way to learn JavaScript";
+// alert(book);
 // Qno.9
-var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
-alert(design);
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
 
 
 
